77d54589120a840ff54fe4c6d19332316b1c62a4
Simon Støvring <simonstoevring@gmail.com>
2016-01-21 19:06:02 +0100

    Handles URLs ending with a slash

--- client/utils.js
+++ client/utils.js
@@ -27,6 +27,10 @@ export function findSlug (programCard) {
 }
 
 export function extractSlugFromUrl (url) {
+  // Handle URLs ending with a slash by removing the slash if it exists
+  if (url.substring(url.length - 1) == '/') {
+    url = url.substring(0, url.length - 1)
+  }
   const parts = url.split('/')
   const last = parts[parts.length - 1]
   return last.replace(/#.*$/, '')
